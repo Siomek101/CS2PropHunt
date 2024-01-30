2d11adf04964ea4a51f21d0583f453578ba654b7
Siomek101 <94121336+Siomek101@users.noreply.github.com>
2024-01-30 20:01:22 +0100

    - Added more spawns for mirage - Made inferno spawns (inferno update)

--- CS2PropHunt.cs
+++ CS2PropHunt.cs
@@ -14,7 +14,7 @@ namespace CS2PropHunt
     {
         public override string ModuleName => "CS2 Prop Hunt Plugin";
 
-        public override string ModuleVersion => "0.0.6";
+        public override string ModuleVersion => "0.1.0";
 
         public List<string> models = new List<string>();
 
@@ -82,17 +82,47 @@ namespace CS2PropHunt
 
                 if (entity.DesignerName == "info_player_terrorist")
                 {
+                    Vector[] infernoSpawns = new Vector[] {
+                        new Vector(940, 1404, 157),
+                        new Vector(940, 1364, 157),
+                        new Vector(940, 1334, 157),
+                        new Vector(940, 1304, 157),
+                        new Vector(1000, 2915, 192),
+                        new Vector(474, -354, 132),
+                        new Vector(504, -354, 132),
+                        new Vector(534, -354, 132),
+                        new Vector(564, -354, 132),
+                        new Vector(1850, 871, 192),
+                        new Vector(1820, 871, 192),
+                        new Vector(1710, 871, 192),
+
+                    };
+                    Vector[] mirageSpawns = new Vector[] {
+                        new Vector(1766, 660, -160), // oob t spawn
+                        new Vector(1736, 660, -160),
+                        new Vector(1706, 660, -160),
+                        new Vector(1676, 660, -160),
+                        new Vector(806, -2244, 232), // Palace A site
+                        new Vector(776, -2244, 232),
+                        new Vector(746, -2244, 232),
+                        new Vector(1007, -957, 64), // OOB t spawn but cool
+                        new Vector(30, -994, -23), // 2 trees at mid
+                        new Vector(-1990, -905, 8), // i like oob
+
+                    };
                     var spawn = new CInfoPlayerTerrorist(entity.Handle);
 
                     if (Server.MapName == "de_mirage")
                     {
-                        spawn.Teleport(new Vector(1766 - spawnTerroristOffset, 660, -160), new QAngle(0, 0, 0), new Vector(0, 0, 0));
+                        if (spawnTerroristOffset > mirageSpawns.Length) spawnTerroristOffset = 0;
+                        spawn.Teleport(mirageSpawns[spawnTerroristOffset], new QAngle(0, 0, 0), new Vector(0, 0, 0));
                     }
                     if (Server.MapName == "de_inferno")
                     {
-                        spawn.Teleport(new Vector(940, 1404 - spawnTerroristOffset, 157), new QAngle(0, 0, 0), new Vector(0, 0, 0));
+                        if (spawnTerroristOffset > infernoSpawns.Length) spawnTerroristOffset = 0;
+                        spawn.Teleport(infernoSpawns[spawnTerroristOffset], new QAngle(0, 0, 0), new Vector(0, 0, 0));
                     }
-                    spawnTerroristOffset += 30;
+                    spawnTerroristOffset ++;
 
 
                 }
